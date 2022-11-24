549e34a58655c5f61cd46c5bedeb44318428e41b
ForSign <tim.shiryaev@gmail.com>
2022-11-24 23:51:56 +0300

    add tests

--- src/lab1/branch_border.py
+++ src/lab1/branch_border.py
@@ -72,47 +72,15 @@ def branch_border(c, A, b, bnd):
     return x_opt
 
 
-test_1 = (
-    # c
-    [-1, -1],
-    # A
-    [[2, 11],
-     [1, 1],
-     [4, -5]],
-    # b
-    [38, 7, 5],
-    # bnd
-    [(0, np.inf),
-     (0, np.inf)])
-
-test_2 = (
-    # c
-    [-7, -9],
-    # A
-    [[-1, 3],
-     [7, 1]],
-    # b
-    [6, 35],
-    # bnd
-    [(0, np.inf),
-     (0, np.inf)])
-
-test_3 = (
-    # c
-    [-2, -3],
-    # A
-    [[3, 4],
-     [2, 5]],
-    # b
-    [24, 22],
-    # bnd
-    [(0, np.inf),
-     (0, np.inf)])
-
-
 if __name__ == '__main__':
-    DEBUF_PRINT = False
-    assert all( branch_border(*test_1) == [3, 2] )
-    assert all( branch_border(*test_2) == [4, 3] )
-    assert all( branch_border(*test_3) == [5, 2] )
-    print("Asserts passed")
+    print(branch_border(*(
+        # c
+        [-1, -1],
+        # A
+        [[5, 9],
+         [9, 5]],
+        # b
+        [63, 63],
+        # bnd
+        [(1, 6),
+         (1, 6)])))
