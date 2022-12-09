7604c31f7cc05cfb5efc708882786ab158e734f7
ForSign <tim.shiryaev@gmail.com>
2022-12-09 20:23:11 +0300

    lab 4

--- tests/test_resource_allocation.py
+++ tests/test_resource_allocation.py
@@ -5,8 +5,12 @@ def test_resource_allocation():
     c = 5
     n = 3
 
-    g = [[1, 2, 3, 4, 5],
-         [0, 1, 2, 4, 7],
-         [2, 2, 3, 3, 5]]
+    y = [0, 1, 2, 3, 4, 5]
+    g = [[0, 1, 2, 3, 4, 5],
+         [0, 0, 1, 2, 4, 7],
+         [0, 2, 2, 3, 3, 5]]
 
-    assert resource_allocation(g, c, n) == [3, 0, 1]
+    assert resource_allocation(y, g, n, c) == [0, 5, 0]
+
+    c = 4
+    assert resource_allocation(y, g, n, c) == [1, 0 ,3]
